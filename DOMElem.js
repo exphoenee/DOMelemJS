06a5de60f38eb9e7c38ea2aaff982a34459dbbc2
exphoenee <bozzay.viktor@gmail.com>
2021-10-17 16:00:26 +0200

    Elem name corrected

--- DOMElem.js
+++ DOMElem.js
@@ -1,4 +1,4 @@
-let Elem = {
+export const Elem = {
   noSpecChars: function (text, lowercase = false) {
     function replaceAll(string, search, replace) {
       return string.split(search).join(replace);
