3603ab4c6784a164f4ca2dd4799662c25823ed45
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-07-06 12:18:21 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-    library: "domElemJS",
+    library: "domelemjs",
     libraryTarget: "umd",
     globalObject: "this",
     umdNamedDefine: true,
