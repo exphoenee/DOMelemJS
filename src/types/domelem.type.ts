356e722ff2473f949832b071bcca7d40dc496671
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 14:57:02 +0100

    Create domelem.type.ts

---
+++ src/types/domelem.type.ts
@@ -0,0 +1,13 @@
+type optionsType = {
+  tag: string; //
+  content?: string; //
+  text?: string; //
+  attrs?: object; //
+  style?: any; //
+  children?: any; //
+  parent?: any; //
+  handleEvent?: any; //
+  append?: boolean; //
+};
+
+export default optionsType;
