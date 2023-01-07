8402566e42a45bc9b7f57bf0b57006db57479457
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 14:57:15 +0100

    Update makeThatArray.ts

--- src/utils/makeThatArray.ts
+++ src/utils/makeThatArray.ts
@@ -1,5 +1,6 @@
 "use strict";
 /* check is it is array do nothing, if not make it array */
-export default function makeThatArray(arr) {
+// this function gets a type or an array of type and returns an array of type
+export default function makeThatArray<T>(arr: T | T[]): T[] {
   return arr ? (Array.isArray(arr) ? arr : [arr]) : [];
 }
